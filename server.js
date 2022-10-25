90a8c76848d1341d4150f9b6cbb587a5ee2c9144
Winston Tang <winstontang@Winstons-MBP.attlocal.net>
2022-10-24 21:38:00 -0700

    feat: middleware setup

--- server.js
+++ server.js
@@ -2,18 +2,43 @@ const express = require("express")
 const pokemon = require("./models/pokemon")
 const app = express()
 const PORT = 3000
+const methodOverride = require("method-override")
+
+app.use(express.urlencoded({extended : false}));
+app.use(methodOverride("_method"));
 
 // Index New Delete Update Create Edit Show
 
 
-app.get('/', (req, res) => {
-    res.send('Home page')
-})
+// app.get('/', (req, res) => {
+//     res.send('Home page')
+// })
 
 app.get('/pokemon', (req, res) => {
     res.render('index.ejs', {
         pokemons:pokemon,
     })
+})
+
+app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+app.post('/pokemon', (req, res) => {
+    console.log(req.body)
+    let stats = {
+        hp: req.body.hp,
+        attack: req.body.attack,
+        defense: req.body.defense,
+    }
+    let createdPokemon = {
+        name: req.body.name, 
+        type: req.body.type,
+        img: req.body.img,
+        stats: stats,
+    }
+    pokemon.push(createdPokemon)        //! adding 'new' pokemon into list of pokemon.js
+    res.redirect('/pokemon')
 })
 
 app.get('/pokemon/:id', (req, res) => {
@@ -23,6 +48,11 @@ app.get('/pokemon/:id', (req, res) => {
     })                                      //! : means whatever the user is inputting
                                             //! pokemons is the variable you've created
 })
+
+
+
+
+
 
 
 
